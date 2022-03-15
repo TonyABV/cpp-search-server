42bc289d8ff0cef9d21d190712eb3aeee4e34a4e
TonyMafioznik <93570389+TonyMafioznik@users.noreply.github.com>
2022-03-15 16:10:42 +0700

    Update log_duration.h

--- search-server/log_duration.h
+++ search-server/log_duration.h
@@ -17,17 +17,9 @@ public:
     // с помощью using для удобства
     using Clock = std::chrono::steady_clock;
 
-    explicit LogDuration(const std::string& stage):stage_(stage) {
-    }
+    explicit LogDuration(const std::string& stage):stage_(stage);
 
-    ~LogDuration() {
-        using namespace std::chrono;
-        using namespace std::literals;
-
-        const auto end_time = Clock::now();
-        const auto dur = end_time - start_time_;
-        std::cerr <<stage_<< ": "s << duration_cast<milliseconds>(dur).count() << " ms"s << std::endl;
-    }
+    ~LogDuration();
 
 private:
     const Clock::time_point start_time_ = Clock::now();
