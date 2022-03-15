cec6953941501726bbecf7548a327a9b6cbe1594
TonyMafioznik <93570389+TonyMafioznik@users.noreply.github.com>
2022-03-15 15:00:58 +0700

    Add files via upload

--- search-server/request_queue.cpp
+++ search-server/request_queue.cpp
@@ -2,19 +2,19 @@
 
 using namespace std;
 
-    RequestQueue::RequestQueue(const SearchServer& search_server) :server_(search_server)  {
-    }
+RequestQueue::RequestQueue(const SearchServer& search_server) :server_(search_server)  {
+}
 
-    vector<Document> RequestQueue::AddFindRequest(const string& raw_query, DocumentStatus status) {
-        return RequestQueue::AddFindRequest(raw_query, [status](int document_id,
-                                                 DocumentStatus document_status, int rating) {
-                                                    return document_status == status;});
-    }
+vector<Document> RequestQueue::AddFindRequest(const string& raw_query, DocumentStatus status) {
+    return RequestQueue::AddFindRequest(raw_query, [status](int document_id,
+                                                DocumentStatus document_status, int rating) {
+                                                return document_status == status;});
+}
 
-    vector<Document> RequestQueue::AddFindRequest(const string& raw_query) {
-        return RequestQueue::AddFindRequest(raw_query, DocumentStatus::ACTUAL);
-    }
+vector<Document> RequestQueue::AddFindRequest(const string& raw_query) {
+    return RequestQueue::AddFindRequest(raw_query, DocumentStatus::ACTUAL);
+}
 
-    int RequestQueue::GetNoResultRequests() const {
-        return count(requests_.begin(),requests_.end(),1);// напишите реализацию
-    }
+int RequestQueue::GetNoResultRequests() const {
+    return count(requests_.begin(),requests_.end(),1);// напишите реализацию
+}
